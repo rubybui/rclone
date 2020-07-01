35a91a53cb3d400fad5837abb1f330e0183a61d0
RubyBui <bui00002@rangers.uwp.edu>
2020-07-01 15:58:10 -0500

    Sign-in

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
+  get "/signup" => "users#new"
+  post "/users" => "users#create" 
+  get "/signin" => "sessions#new"
+  get "/signout"=> "session#destroy", via: [:delete]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
